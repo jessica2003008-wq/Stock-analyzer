8b9739ddd91194b1984388365590f26c7dfa455b
jessica2003008-wq <jessica2003008@gmail.com>
2026-03-05 14:55:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import sys
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 import config
+anthropic_key = st.secrets.get("ANTHROPIC_API_KEY", "")
 from data.yfinance_client import YFinanceClient, YFinanceError
 from data.edgar_client import EdgarClient
 from llm.claude_client import ClaudeClient, LLMError
